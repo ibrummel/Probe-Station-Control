49350e0ffcc3d9352fcd84185b9bf47076884a37
Ian B <ian.brummel@gmail.com>
2019-11-24 23:38:21 -0500

    Created main window class to house different measurement widgets.

---
+++ Main_App.py
@@ -0,0 +1,44 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget
+import visa
+from pyvisa.errors import VisaIOError
+from Agilent_E4980A import AgilentE4980A
+from Sun_EC1X import SunEC1xChamber
+from Cap_Freq import CapFreqWidget
+from Cap_Freq_Temp import CapFreqTempWidget
+
+
+class ProbeStationControlMainWindow(QMainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.instruments = {'lcr': 'GPIB0::18::INSTR',
+                            'sun': 'GPIB0::6::INSTR',
+                            'src_meas': 'NOT USED',
+                            'electrometer': 'NOT USED'}
+
+        self.rm = visa.ResourceManager()
+
+        # Connect to instruments, eventually replace with the dialog to set ID strings
+        self.instruments['lcr'] = AgilentE4980A(parent=self, gpib_addr=self.instruments['lcr'])
+        self.instruments['sun'] = SunEC1xChamber(parent=self, gpib_addr=self.instruments['sun'])
+        # Add connections to other instruments as needed.
+
+        self.cap_freq = CapFreqWidget(self.instruments['lcr'])
+        self.cap_freq_temp = CapFreqTempWidget(self.instruments['lcr'], self.instruments['sun'])
+
+        self.tabs_meas_types = QTabWidget()
+        self.tabs_meas_types.addTab(self.cap_freq, 'Capacitance-Frequency')
+        self.tabs_meas_types.addTab(self.cap_freq_temp, 'Capacitance-Frequency-Temperature')
+
+        self.setCentralWidget(self.tabs_meas_types)
+
+
+app = QApplication(sys.argv)
+
+main_window = ProbeStationControlMainWindow()
+main_window.show()
+sys.exit(app.exec_())
+
